850c43594882aa5e94febafc7d7a1c0c452a2816
Dharani T <gct.it18.dharani@gmail.com>
2022-01-01 12:35:23 +0530

    Twelve Days

---
+++ twelveDays.java
@@ -0,0 +1,87 @@
+class TwelveDays {
+    String[] days = {
+            "first", "second", "third", "fourth", "fifth", "sixth",
+            "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"
+    };
+    
+    String[] lyricsTwelveDays = {
+            "a Partridge in a Pear Tree",
+            "two Turtle Doves",
+            "three French Hens",
+            "four Calling Birds",
+            "five Gold Rings",
+            "six Geese-a-Laying",
+            "seven Swans-a-Swimming",
+            "eight Maids-a-Milking",
+            "nine Ladies Dancing",
+            "ten Lords-a-Leaping",
+            "eleven Pipers Piping",
+            "twelve Drummers Drumming"
+    };
+    
+     String lyricsFirstPartStart = "On the ";
+     String lyricsFirstPartEnd = " day of Christmas my true love gave to me: ";
+    
+    String verse(int verseNumber) {
+      // Solution for TwelveDays
+      
+        StringBuilder sb = new StringBuilder();        
+        sb.append(lyricsFirstPartStart);
+        sb.append(days[verseNumber - 1]);
+        sb.append( lyricsFirstPartEnd);
+        
+        for (int i = verseNumber; i > 0; i--) {
+            sb.append(lyricsTwelveDays[i - 1]);
+            if (i != 1) {
+                sb.append(", ");
+            }
+            if (i == 2) {
+                sb.append("and ");
+            }
+        }
+        
+        sb.append(".\n");
+        return sb.toString();
+    }
+
+    String verses(int startVerse, int endVerse) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = startVerse; i <= endVerse; i++) {
+            sb.append(verse(i));
+            if (i != endVerse){
+              sb.append("\n");
+        }
+        return sb.toString();
+    }
+    
+    String sing() {
+        return verses(1, 12);
+    }
+}
+  
+  
+/*  OUTPUT
+
+On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
+
+On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree. */
